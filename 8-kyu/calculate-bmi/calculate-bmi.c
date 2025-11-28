ccaa1ddc6667c3d27afdda5e0e889f2bda2e41f3
00ill <101080818+00ill@users.noreply.github.com>
2025-11-28 13:49:35 +0900

    Add solution - CodeHub

---
+++ 8-kyu/calculate-bmi/calculate-bmi.c
@@ -0,0 +1,12 @@
+/*    N.B. do not allocate memory,
+   instead: return a string literal  */
+​
+const char *bmi (int weight, double height)
+{
+  double bmi = weight / (height * height);
+  if(bmi <= 18.5) return "Underweight";
+  if(bmi <= 25) return "Normal";
+  if(bmi <= 30) return "Overweight";
+  return "Obese";
+}
+​
