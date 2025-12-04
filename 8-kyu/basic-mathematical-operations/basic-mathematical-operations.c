4021aecbda7a150f4d1dfc2a3bb24896f2b77db7
00ill <101080818+00ill@users.noreply.github.com>
2025-12-05 08:45:53 +0900

    Add solution - CodeHub

---
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.c
@@ -0,0 +1,6 @@
+int basic_op(char op, int value1, int value2) {
+  if(op == '+') return value1 + value2;
+  if(op == '-') return value1 - value2;
+  if(op == '*') return value1 * value2;
+  if(op == '/') return value1 / value2;
+}
